184d6ccde3edd5c7617196b5595919645abf03f0
Raj Patil <floatraj@gmail.com>
2020-08-22 01:31:08 +0530

    Partially Completed video text editor able to generate html and add to dom

--- src/app/components/video-editor/video-editor.component.ts
+++ src/app/components/video-editor/video-editor.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Renderer2, ElementRef, ViewChild, AfterViewInit} from '@angular/core';
 
 @Component({
   selector: 'app-video-editor',
   templateUrl: './video-editor.component.html',
   styleUrls: ['./video-editor.component.css']
 })
-export class VideoEditorComponent implements OnInit {
+export class VideoEditorComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
-
+  constructor(private renderer: Renderer2, private elRef: ElementRef) { }
+  @ViewChild('transcript')
+  transcript: ElementRef;
   ngOnInit(): void {
+    // const initialSpan = this.renderer.createElement('span');
+    // const text = this.renderer.createText('Hi hello');
+    // this.renderer.appendChild(initialSpan, text);
+    // this.renderer.appendChild(this.transcript.nativeElement, initialSpan);
+    // console.log(this.transcript.nativeElement);
+  }
+
+  ngAfterViewInit(): void {
+    // console.log(this.transcript.nativeElement);
+    const initialSpan = this.renderer.createElement('span');
+    const text = this.renderer.createText('Hi hello');
+    this.renderer.appendChild(initialSpan, text);
+    this.renderer.appendChild(this.transcript.nativeElement, initialSpan);
+    const initialSpan1 = this.renderer.createElement('span');
+    const text1 = this.renderer.createText('Hi hello');
+    this.renderer.appendChild(initialSpan1, text1);
+    this.renderer.appendChild(this.transcript.nativeElement, initialSpan1);
   }
 
 }
